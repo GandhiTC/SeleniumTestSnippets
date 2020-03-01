a8616ceaea98e1c239eb486e3ef93d555fcf5b3e
GandhiTC <GandhiTC@gmail.com>
2020-03-01 02:31:12 -0600

    added ThreadLocal test

--- src/test/java/com/github/GandhiTC/java/SeleniumTestSnippets/code/ThreadedActions.java
+++ src/test/java/com/github/GandhiTC/java/SeleniumTestSnippets/code/ThreadedActions.java
@@ -11,25 +11,43 @@ public class ThreadedActions
 {
 	public static void main(String[] args)
 	{
+		//	Test 1	-	Get the id of the thread thats running this script
+		System.out.println("Thread ID running main()          \t\t: " + Thread.currentThread().getId());
+		
+		
+		
+		//	Test 2	-	Get the id of a new thread that invokes a new Runnable()
 		Thread newThread = new Thread(new Runnable()
 		{
 			@Override
 			public void run()
 			{
-				
+				//	do something
 			}
 		});
+		
 		newThread.start();
 		
+		System.out.println("Thread ID running a new thread    \t\t: " + newThread.getId());
 		
+		
+		
+		//	Test 3	-	Get the id of a new thread that invokes an existing class
 		ThreadedIncrementer	incrementer	= new ThreadedIncrementer(0);
 		Thread 				classThread	= new Thread(incrementer);
+		
 		classThread.start();
+		
+		System.out.println("Thread ID running a class thread  \t\t: " + classThread.getId());
+		
+		
 		
 //		new Thread(MyClass::doWork).start();
 //		new Thread(() -> doWork(someParam));
 		
 		
+		
+		//	Test 4	-	Get the id of a new thread thats running a static method in this class
 		Thread staticMethodThread = new Thread(new Runnable()
 		{
 			@Override
@@ -38,14 +56,12 @@ public class ThreadedActions
 				staticMethod();
 			}
 		});
+		
 		staticMethodThread.start();
 		
 		
-		System.out.println("Thread ID running main()          \t\t: " + Thread.currentThread().getId());
-		System.out.println("Thread ID running a new thread    \t\t: " + newThread.getId());
-		System.out.println("Thread ID running a class thread  \t\t: " + classThread.getId());
 		
-		
+		//	Test 5	-	Get the id's of new threads managed by an executor service that uses a fixed thread pool
 		final int		numThreads		= 5;
 		ExecutorService	execService1	= Executors.newFixedThreadPool(numThreads);
 
@@ -66,10 +82,13 @@ public class ThreadedActions
 		execService1.shutdown();
 		
 		
+		
 		//	good for a quick one-timer
 //		Executors.newSingleThreadExecutor().submit(someClass::someMethod);
 		
 		
+		
+		//	Test 6	-	Get the id of a new thread managed by a single thread executor service
 		ExecutorService	execService2	= Executors.newSingleThreadExecutor();
 				
 		execService2.submit(new Runnable()
@@ -82,6 +101,65 @@ public class ThreadedActions
 		});
 		
 		execService2.shutdown();
+		
+		
+		
+		//	Test 7	-	Get id's of new threads which call on a class that uses a ThreadLocal variable.
+		//				1)  Before : create variables and print to console the thread id and randomly generated integer
+		//				2)  During : using a new thread, re-assign the same variables, then print to console again - values should differ from step 1 values
+		//				3)	After  : without re-assigning the variables, print to console again - values should match step 1 values
+		ThreadLocalRandInt 	randInt1	= new ThreadLocalRandInt();
+		ThreadLocalRandInt 	randInt2	= new ThreadLocalRandInt();
+		
+		Thread 				randCls1	= new Thread(randInt1);
+		Thread 				randCls2	= new Thread(randInt1);
+		
+		randCls1.start();
+		randCls2.start();
+		
+		//	1)  Before
+		System.out.println("randInt1");
+		System.out.println("\tThread ID (Before) = " + randCls1.getId());
+		System.out.println("\tRandom #  (Before) = " + randInt1.value());
+		
+		System.out.println("randInt2");
+		System.out.println("\tThread ID (Before) = " + randCls2.getId());
+		System.out.println("\tRandom #  (Before) = " + randInt2.value());
+		
+		//	2)	During
+		Thread addedThread = new Thread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				ThreadLocalRandInt 	randInt1	= new ThreadLocalRandInt();
+				ThreadLocalRandInt 	randInt2	= new ThreadLocalRandInt();
+				
+				Thread 				randCls1	= new Thread(randInt1);
+				Thread 				randCls2	= new Thread(randInt1);
+				
+				randCls1.start();
+				randCls2.start();
+				
+				System.out.println("randInt1");
+				System.out.println("\tThread ID (During) = " + randCls1.getId());
+				System.out.println("\tRandom #  (During) = " + randInt1.value());
+				
+				System.out.println("randInt2");
+				System.out.println("\tThread ID (During) = " + randCls2.getId());
+				System.out.println("\tRandom #  (During) = " + randInt2.value());
+			}
+		});
+		addedThread.start();
+		
+		//	3)	After
+		System.out.println("randInt1");
+		System.out.println("\tThread ID (After) = " + randCls1.getId());
+		System.out.println("\tRandom #  (After) = " + randInt1.value());
+		
+		System.out.println("randInt2");
+		System.out.println("\tThread ID (After) = " + randCls2.getId());
+		System.out.println("\tRandom #  (After) = " + randInt2.value());
 	}
 	
 	

719c0df0f7ca45990c0cd15ad02178a56a8946f6
GandhiTC <GandhiTC@gmail.com>
2020-03-01 02:31:56 -0600

    initial commit

--- src/test/java/com/github/GandhiTC/java/SeleniumTestSnippets/code/Tables_ChildElements.java
+++ src/test/java/com/github/GandhiTC/java/SeleniumTestSnippets/code/Tables_ChildElements.java
@@ -2,11 +2,13 @@ package com.github.GandhiTC.java.SeleniumTestSnippets.code;
 
 
 
+/*
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+*/
 
 
 
